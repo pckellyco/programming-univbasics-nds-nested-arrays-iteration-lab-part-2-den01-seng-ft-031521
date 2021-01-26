1cf44ac72af9ef4eaf0bccb51080d992e52c8ea2
Paige Kelly <pckelly.co@gmail.com>
2021-01-26 00:38:53 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,9 +1,17 @@
+# use helper function to find min num in sub-array
+# create a variable min_num to store the min_num
+# iterate through sub-array
+# identify each num in sub-array
+# compare each num to min_num
+# if num < min_num then min_num = num
+# return min_num
+
 # identify the sub_arrays
-# find min num in sub_array
-# create a variable = min to store the min value
-# compare all nums/ele in sub-array to min
-# push min into new array
-# use helper function to find min value in sub-array
+# create empty array_with_min_values
+# run each sub-array through helper function
+# push value of min_value(sub_array) to array_with_min_values
+# return array_with_min_values
+
 
 def min_value(array)
   idx = 0
@@ -23,47 +31,13 @@ def find_min_in_nested_arrays(src)
  sub_array_idx = 0
  while sub_array_idx < src.length do
    sub_array = src[sub_array_idx]
-   num_idx = 0
-   minimum_value = sub_array[0]
+   min_value_in_sub_array = min_value(sub_array)
+   array_with_min_values.push(min_value_in_sub_array)
    sub_array_idx += 1
-   while num_idx < sub_array.length do
-     num = sub_array[num_idx]
-     if num < minimum_value do
-       minimum_value = num
-     end
-     num_idx += 1
-   end
-   array_with_min_values.push(minimum_value)
-   array_with_min_values
  end
+ array_with_min_values
+end
  
-def find_min_in_nested_arrays(src)
- array_with_min_values = []
- sub_array_idx = 0
  
- while sub_array_idx < src.length {
-   sub_array = src[sub_array_idx]
-   num_idx = 0
-   minimum_value = sub_array[0]
-   sub_array_idx += 1
-   while num_idx < sub_array.length {
-     num = sub_array[num_idx]
-     if num < minimum_value {
-       minimum_value = num
-     }
-     num_idx += 1
-     array_with_min_values.push(minimum_value)
-   }
-   
- }
-   
-   
-     if num < minimum_value do
-       minimum_value = num
-     end
-     num_idx += 1
-   end
-   array_with_min_values.push(minimum_value)
-   array_with_min_values
- end
+
   
